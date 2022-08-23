075838dfb310d311981b2d705026c761dd225c1e
Melis Augsburger <melis.augsburger@outlook.com>
2022-08-23 10:04:46 +0200

    Login component created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { Router } from 'express';
+import { Routes } from 'react-router-dom';
 import './App.css';
+import Login from './Login';
 
-function App() {
+export default function App() {
   return (
-    ""
+    <Router>
+      <Routes>
+      <Route path="/" element={<Login />} />
+      </Routes>
+    </Router>
   )
 }
-
-export default App;

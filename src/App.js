08e9dabcdc8cca58b25206b46efa3c44ce43e31a
Melis Augsburger <melis.augsburger@outlook.com>
2022-08-25 17:38:45 +0200

    draft of menu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Collections from './Components/Collections';
+import Menu from './Components/Menu';
 import Sign from './Components/Sign';
 import Signin from './Components/Signin'
 import Signup from './Components/Signup'
@@ -12,7 +13,12 @@ export default function App() {
           <Route path="/signup" element={<Signup />}/>
           <Route path="/" element={<Signin />}/>
         </Route>
-        <Route path="/collections" element={<Collections/>} />
+        <Route>
+          <Route path="/collections" element={<Collections/>} />
+        </Route>
       </Routes>
+/*       <Routes>
+        <Route path="/" element={<Menu />}></Route>
+      </Routes> */
   )
 }
---
+++ src/Components/Menu.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../css/menu.css'
+
+export default function Menu() {
+  return (
+    <div className='menu'>
+        <img src="/images/goback.svg" alt="goback" />
+        <img src="/images/plus.svg" alt="plus" />
+        <img src="/images/magnifyingglass.svg" alt="magnifyingglass" />
+    </div>
+  )
+}

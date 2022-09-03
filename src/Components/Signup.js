ed5dfd165db1deae52b207f37fecf4b3d47c53dd
Melis Augsburger <melis.augsburger@outlook.com>
2022-09-03 12:10:23 +0200

    registration of user function finalized

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,13 +1,51 @@
-import React from 'react'
-import '../css/signup.css'
+import axios from "axios";
+import React, { useRef } from "react";
+import { useNavigate } from "react-router";
+import "../css/signup.css";
 
 export default function Signup() {
+  const usernameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const navigate = useNavigate();
+
+  function signup() {
+    axios.post("/", {
+      data: {
+        username: usernameRef,
+        email: emailRef,
+        password: passwordRef,
+      },
+    });
+    navigate("/collections");
+  }
   return (
-    <div className="signup">
-      <input placeholder='Username' name='username' className='input'/>
-      <input placeholder='E-Mail' name='email' className='input'/>
-      <input placeholder='Password' name='password' className='input'/>
-      <button className="buttonSignUP">Sign Up</button>
-    </div>
-  )
+    <form onSubmit={signup}>
+      <div className="signup">
+        <input
+          ref={usernameRef}
+          placeholder="Username"
+          name="username"
+          className="input"
+        />
+        <input
+          ref={emailRef}
+          placeholder="E-Mail"
+          name="email"
+          className="input"
+        />
+        <input
+          ref={passwordRef}
+          placeholder="Password"
+          name="password"
+          type="password"
+          className="input"
+        />
+        <button type="submit" className="buttonSignUP">
+          Sign Up
+        </button>
+      </div>
+    </form>
+  );
 }

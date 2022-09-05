a1b388aca4d88fe70de8d3d44a88927733244645
Melis Augsburger <melis.augsburger@outlook.com>
2022-09-05 17:41:41 +0200

    get all collections of user

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Collections from "./Components/Collections";
 import Collection from "./Components/Collection";
-import Menu from "./Components/Menu";
 import Sign from "./Components/Sign";
 import Signin from "./Components/Signin";
 import Signup from "./Components/Signup";
 import Item from "./Components/Item";
 import Settings from "./Components/Settings";
-import Scan from "./Components/Scan";
 import AddItem from "./Components/AddItem";
 import { useState } from "react";
 
@@ -26,12 +24,15 @@ export default function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/" element={<Signin />} />
       </Route>
-      <Route path="/collections" element={<Collections />} />
+      <Route path="/users/1/collections" element={<Collections />} />
       <Route
-        path="/collections/:collectionId"
+        path="/users/1/collections/:collectionId"
         element={<Collection setScannedData={setScannedData} />}
       />
-      <Route path="/collections/:collectionId/:itemId" element={<Item />} />
+      <Route
+        path="/users/1/collections/:collectionId/:itemId"
+        element={<Item />}
+      />
       <Route path="/settings" element={<Settings />}></Route>
       <Route
         path="/collectionts/:collectionId/addItem"
--- src/Components/Collections.js
+++ src/Components/Collections.js
@@ -1,20 +1,32 @@
-import React from 'react'
-import '../css/collections.css'
-import Card from './Card'
-import Menu from './Menu'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "../css/collections.css";
+import Card from "./Card";
+import Menu from "./Menu";
 
 export default function Collections() {
+  const [collections, setCollections] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8000/users/1/collections`)
+      .then((response) => response.json())
+      .then((data) => setCollections(data));
+  }, []);
+
   return (
     <>
       <div className="yourCollections">
         <h1>Your collections</h1>
       </div>
       <div className="wrapper">
-        <Card />
+        {collections.map((collection, i) => (
+          <Card id={i + 1} key={i} name={collection.title} />
+        ))}
       </div>
-      <div className='menu'>
+      <div className="menu">
         <Menu />
       </div>
     </>
-  )
+  );
 }

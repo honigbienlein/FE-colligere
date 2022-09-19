31754c9990d914fcfa18328bce641038d7273826
Nettester <github@gluch.eu>
2022-09-19 10:57:28 +0200

    changed className with camelCase

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,52 +1,49 @@
-import { render } from "@testing-library/react";
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router";
-import { NavLink } from "react-router-dom";
-import { HEROKUURL } from "../environmentVariables";
-import Card from "./Card";
-import "../css/search.css";
+import { render } from '@testing-library/react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router'
+import { NavLink } from 'react-router-dom'
+import { HEROKUURL } from '../environmentVariables'
+import Card from './Card'
+import '../css/search.css'
 
 export default function Search() {
-  const { userId, input } = useParams();
-  const [items, setItems] = useState({});
+	const { userId, input } = useParams()
+	const [items, setItems] = useState({})
 
-  // Get all items of user
-  useEffect(() => {
-    axios
-      .get(`${HEROKUURL}/users/${userId}/items/${input}`)
-      .then((response) => {
-        setItems(response.data);
-      });
-  }, []);
+	// Get all items of user
+	useEffect(() => {
+		axios.get(`${HEROKUURL}/users/${userId}/items/${input}`).then(response => {
+			setItems(response.data)
+		})
+	}, [])
 
-  return (
-    <>
-      <div className="search">
-        <h1>You were searching for: {input}</h1>
-      </div>
-      <div className="wrapper">
-        {items?.entries?.map((item, i) => {
-          console.log(items);
-          return (
-            <NavLink
-              to={`/users/${userId}/collections/${item?.collectionId}/items/${item?.entryId}`}
-            >
-              <Card
-                id={item.entryId}
-                key={item.entryId}
-                title={item.title}
-                cover={item.cover}
-              />
-            </NavLink>
-          );
-        })}
-      </div>
-      <div className="goback">
-        <NavLink to={`/users/${userId}/collections/`}>
-          <img src="/images/goback.svg" alt="goback" width="16" />
-        </NavLink>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className='search'>
+				<h1>You were searching for: {input}</h1>
+			</div>
+			<div className='wrapper'>
+				{items?.entries?.map((item, i) => {
+					console.log(items)
+					return (
+						<NavLink
+							to={`/users/${userId}/collections/${item?.collectionId}/items/${item?.entryId}`}>
+							<Card
+								id={item.entryId}
+								key={item.entryId}
+								title={item.title}
+								cover={item.cover}
+							/>
+						</NavLink>
+					)
+				})}
+			</div>
+			<div className='goBack'>
+				<NavLink to={`/users/${userId}/collections/`}>
+					<img src='/images/goback.svg' alt='goback' width='16' />
+				</NavLink>
+			</div>
+		</>
+	)
 }

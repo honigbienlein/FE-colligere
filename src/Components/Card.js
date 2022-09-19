b375fd4248df1edf6fb69d440a19a80b9f67b899
Nettester <github@gluch.eu>
2022-09-19 10:48:04 +0200

    changed className with camelCase

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,18 +1,18 @@
-import React from "react";
-import "../css/card.css";
+import React from 'react'
+import '../css/card.css'
 
 export default function Card({ title, cover }) {
-  const backgroundImg = {
-    backgroundImage: `url('${cover ?? "/images/book.jpeg"}')`,
-  };
+	const backgroundImg = {
+		backgroundImage: `url('${cover ?? '/images/book.jpeg'}')`,
+	}
 
-  return (
-    <>
-      <div className="card" style={backgroundImg}>
-        <div className="card-title">
-          <p>{title}</p>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className='card' style={backgroundImg}>
+				<div className='cardTitle'>
+					<p>{title}</p>
+				</div>
+			</div>
+		</>
+	)
 }
